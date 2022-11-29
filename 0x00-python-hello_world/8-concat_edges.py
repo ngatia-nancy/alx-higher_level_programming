6f2761ca88eea77db394b75c0b9a7fb227aa5e39
ngatia-nancy <nancyngatia1@gmail.com>
2022-11-29 21:48:55 +0000

    concat-edges

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -2,8 +2,4 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 str = str[39:66] + str[106:112] + str[:6]
-<<<<<<< HEAD
 print(str)
-=======
-print(str)
->>>>>>> 0984d13c431908981cffa8325ac485451c8014ab
