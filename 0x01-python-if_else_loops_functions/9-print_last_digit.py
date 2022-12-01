3f4967bad47d04c51a27bc603d9268dd2284a31f
ngatia-nancy <nancyngatia1@gmail.com>
2022-12-01 02:29:05 +0000

    last digit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
+
 def print_last_digit(number):
-    if number < 0:
-        number = ((-1) * number) % 10
-    else:
-        number = number % 10
-    print(f"{number}", end="")
-    return
+    print(abs(number) % 10, end="")
+    return (abs(number) % 10)
