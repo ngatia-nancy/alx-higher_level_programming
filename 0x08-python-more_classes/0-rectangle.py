0cbe1fb1c15879ac4c18c7270ff0a5a6dc820b4c
ngatia-nancy <nancyngatia1@gmail.com>
2023-01-05 20:09:31 +0000

    Task 0

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -3,10 +3,5 @@
 
 
 class Rectangle:
-<<<<<<< HEAD
     """Represent a rectangle."""
     pass
-=======
-        """Represent a rectangle."""
-            pass
->>>>>>> 69acfbb7ebe0d9c36f74a0b4998122aee4e51845
