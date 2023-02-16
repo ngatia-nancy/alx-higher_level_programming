4c073866ee861575e69fdf8f00ef8f159c2f6a05
ngatia-nancy <nancyngatia1@gmail.com>
2023-02-16 12:47:49 -0800

    a script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server
+-- not allowed to use the DESCRIBE or EXPLAIN statements
+
+SHOW CREATE DATABASE hbtn_0c_0;
