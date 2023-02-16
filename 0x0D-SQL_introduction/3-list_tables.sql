1a994e6c248795b56551d16ba0204511a3b0172a
ngatia-nancy <nancyngatia1@gmail.com>
2023-02-16 12:38:36 -0800

    a script that creates a table called first_table in the current database in your MySQL server.

---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,3 @@
+-- script that lists all the tables of a database in your MySQL server
+-- database name will be passed as argument of mysql command
+SHOW TABLES;
---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+-- script that creates a table called first_table in the current database in your MySQL server. 
+-- current data base => hbtn_0c_0
+
+CREATE TABLE IF NOT EXISTS first_table(
+	id INT,
+	name VARCHAR(256)
+	);
