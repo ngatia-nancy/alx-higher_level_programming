6e2e4fd938dd1ae008a80b434c28e3aba4a20e95
ngatia-nancy <nancyngatia1@gmail.com>
2023-02-16 13:41:25 -0800

    script that creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- script that lists all records of the table second_table of the database hbtn_0c_0
+-- Result set should display both the score and the name (in this order)
+-- Records should be ordered by score (top first)
+
+SELECT score, name FROM second_table ORDER BY score DESC;
