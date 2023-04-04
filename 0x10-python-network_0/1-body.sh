f974a089a6373d8173904fafb6c424d645a7bbb7
ngatia-nancy <nancyngatia1@gmail.com>
2023-04-04 14:20:56 -0700

    Get the response body for a given URL for 200 status code responses

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# script that takes in a URL, sends a GET request to the URL
-# Display only body of a 200 status code response
-
+# script that takes in a URL, sends a GET request to the URL, display only body of a 200 status code response
 curl -sL "$1"
