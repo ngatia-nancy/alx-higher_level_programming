307e2b5bf248e109d0ed2a773def86b6ff3577f9
ngatia-nancy <nancyngatia1@gmail.com>
2023-04-06 10:43:35 -0700

    script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,9 +6,9 @@ from urllib.request import Request, urlopen
 if __name__ == '__main__':
     req = Request('https://alx-intranet.hbtn.io/status')
     with urlopen(req) as response:
-       page = response.read()
-       print("Body response: ")
-       print("\t- type: {}".format(type(page)))
-       print("\t- content: {}".format(page))
-       print("\t- utf8 content: {}".format(page.decode('utf-8')))
+        page = response.read()
+        print("Body response: ")
+        print("\t- type: {}".format(type(page)))
+        print("\t- content: {}".format(page))
+        print("\t- utf8 content: {}".format(page.decode('utf-8')))
 
