48655e4a9983547ee35e2d477e0fd7ed82ab21ed
ngatia-nancy <nancyngatia1@gmail.com>
2023-04-06 13:56:04 -0700

    handling error codes

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -15,4 +15,4 @@ if __name__ == '__main__':
             page = response.read()
             print(page.decode('utf-8'))
     except urllib.error.HTTPError as e:
-        print("Error code: ", e.code)
+        print("Error code:", e.code)
