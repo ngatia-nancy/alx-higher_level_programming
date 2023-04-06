73fdfdc039658e1c931ecf4aac7c9250b144e63d
ngatia-nancy <nancyngatia1@gmail.com>
2023-04-06 11:50:51 -0700

    script that returns header response value

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-"""script that takes in a URL, 
-    sends a request to the URL and 
-    displays the value of the X-Request-Id variable found in the header of the response.
+"""script that takes in a URL,
+   sends a request to the URL,
+   displays the value of the X-Request-Id in header response.
 """
 
-import sys
 import urllib.request
+import sys
 
 if __name__ == '__main__':
     req = urllib.request.Request(sys.argv[1])
     with urllib.request.urlopen(req) as response:
         print(dict(response.headers).get("X-Request-Id"))
+
