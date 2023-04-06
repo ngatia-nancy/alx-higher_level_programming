6ea0c6b7836431345c2e758ea25a64dd3daa28e6
ngatia-nancy <nancyngatia1@gmail.com>
2023-04-06 14:14:45 -0700

    Python script that fetches https://alx-intranet.hbtn.io/status using request package

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Python script that fetches https://alx-intranet.hbtn.io/status.
+   Uses the request package
+"""
+
+import requests
+
+if __name__ == '__main__':
+    with requests.get('https://alx-intranet.hbtn.io/status') as r:
+        response = r.text
+        print("Body response:")
+        print("\t - type: {}".format(type(response)))
+        print("\t - content: {}".format(response))
